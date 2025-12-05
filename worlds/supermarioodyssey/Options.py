e9bd19ecda593ef468127ebd954eba74bf5b2f6a
Game Fire <code@thegamefire.com>
2025-12-05 17:26:16 +0100

    Added More Options for SMO

--- worlds/supermarioodyssey/Options.py
+++ worlds/supermarioodyssey/Options.py
@@ -1,5 +1,48 @@
-from Options import DeathLink, PerGameCommonOptions
+from Options import DeathLink, PerGameCommonOptions, Choice
 
+
+class EndGoal(Choice):
+    """The end goal required to beat the game.
+    Vanilla: Finish the vanilla storyline.
+
+    Dark Side: Beat Dark Side
+
+    Darker Side: Beat Darker Side
+    """
+    display_name = "End Goal"
+    option_vanilla = 1
+    option_dark = 2
+    option_darker = 3
+    default = 1
+
+class KingdomRandomizer(Choice):
+    """Shuffles the order that kingdoms get unlocked.
+    Light means that kingdoms get shuffled but Moon, Dark and Darker still appear last.
+    Insanity means there is no such rules."""
+
+    display_name = "Shuffle Kingdoms"
+    option_false=0
+    option_light = 1
+    option_insanity = 2
+    default = 1
+
+class RandomizeCosmetics(Choice):
+    """Puts Mario's Outfits into the itempool"""
+    display_name = "Randomize Cosmetics"
+    option_false = 0
+    option_true = 1
+    default = 0
+
+class RandomizeMovement(Choice):
+    """Puts Mario's Movement Abilities into the itempool"""
+    display_name = "Randomize Movement"
+    option_false = 0
+    option_true = 1
+    default = 0
 
 class SMOOptions(PerGameCommonOptions):
-    death_link: DeathLink
+    EndGoal: EndGoal
+    KingdomRandomizer:  KingdomRandomizer
+    RandomizeCosmetics: RandomizeCosmetics
+    RandomizeMovement:  RandomizeMovement
+    death_link:         DeathLink
--- worlds/supermarioodyssey/__init__.py
+++ worlds/supermarioodyssey/__init__.py
@@ -7,3 +7,8 @@ class SuperMarioOdysseyWorld(World):
     game = "Super Mario Odyssey"
     options_dataclass = SMOOptions
     options: SMOOptions
+
+
+    def __init__(self, multiworld: "MultiWorld", player: int):
+        super().__init__(multiworld, player)
+
